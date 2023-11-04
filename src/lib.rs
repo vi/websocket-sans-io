24aeef43af9c15bec98467902fe98555d066dcb1
Vitaly _Vi Shukela <vi0oss@gmail.com>
2023-11-04 23:40:01 +0100

    Frame decoder

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,15 @@
 
 #![no_std]
 
-pub struct WebSocketEncoder {
+use nonmax::NonMaxU8;
+
+pub struct WebSocketFrameEncoder {
     buf: [u8; 3],
 }
-impl WebSocketEncoder {
+impl WebSocketFrameEncoder {
     pub fn add_event<'a: 'c, 'b: 'c, 'c>(
         &'a mut self,
-        e: WebsocketEvent,
+        _e: WebsocketEvent,
         payload_chunk: Option<&'b mut [u8]>,
     ) -> Result<&'c [u8], Error> {
         if let Some(c) = payload_chunk {
@@ -23,7 +25,53 @@ impl WebSocketEncoder {
     }
 }
 
-pub struct WebSocketDecoder {}
+#[derive(Clone, Copy)]
+struct SmallBufWithLen<const C: usize> {
+    len: u8,
+    data: [u8; C],
+}
+
+impl<const C: usize> SmallBufWithLen<C> {
+    /// Take as much bytes as possible from the slice pointer, updating it in process
+    fn slurp<'a, 'c>(&'c mut self, data: &'a mut [u8]) -> &'a mut [u8] {
+        let offset = self.len as usize;
+        let maxlen = (C - offset).min(data.len());
+        self.data[offset..maxlen].copy_from_slice(&data[..maxlen]);
+        self.len += maxlen as u8;
+        &mut data[maxlen..]
+    }
+    fn is_full(&self) -> bool {
+        self.len as usize == C
+    }
+    const fn new() -> SmallBufWithLen<C> {
+        SmallBufWithLen {
+            len: 0,
+            data: [0u8; C],
+        }
+    }
+}
+
+type PayloadLength = u64;
+
+/// Represents what data is expected to come next
+#[derive(Clone, Copy)]
+enum FrameDecodingState {
+    HeaderBeginning(SmallBufWithLen<2>),
+    PayloadLength16(SmallBufWithLen<2>),
+    PayloadLength64(SmallBufWithLen<8>),
+    MaskingKey(SmallBufWithLen<4>),
+    PayloadData {
+        phase: Option<NonMaxU8>,
+        remaining: PayloadLength,
+    },
+}
+
+pub struct WebSocketFrameDecoder {
+    state: FrameDecodingState,
+    mask: [u8; 4],
+    basic_header: [u8; 2],
+    payload_length: PayloadLength,
+}
 
 pub struct WebSocketDecoderAddDataResult<'a> {
     /// Data to be fed back into the next invocation of `add_data`.
@@ -34,30 +82,230 @@ pub struct WebSocketDecoderAddDataResult<'a> {
     pub event: Option<WebsocketEvent>,
 }
 
-impl WebSocketDecoder {
+impl<'a> core::fmt::Debug for WebSocketDecoderAddDataResult<'a> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("WebSocketDecoderAddDataResult")
+            .field("unprocessed_input_data.len", &self.unprocessed_input_data.len())
+            .field("decoded_payload.len", &self.decoded_payload.map(|x|x.len()))
+            .field("event", &self.event)
+            .finish()
+    }
+}
+
+impl WebSocketFrameDecoder {
+    fn get_opcode(&self) -> Opcode {
+        use Opcode::*;
+        match self.basic_header[0] & 0xF {
+            0 => Continuation,
+            1 => Text,
+            2 => Binary,
+            3 => ReservedData3,
+            4 => ReservedData4,
+            5 => ReservedData5,
+            6 => ReservedData6,
+            7 => ReservedData7,
+            8 => ConnectionClose,
+            9 => Ping,
+            0xA => Pong,
+            0xB => ReservedControlB,
+            0xC => ReservedControlC,
+            0xD => ReservedControlD,
+            0xE => ReservedControlE,
+            0xF => ReservedControlF,
+            _ => unreachable!(),
+        }
+    }
+
+    fn get_frame_info(&self, masked: bool) -> FrameInfo {
+        FrameInfo {
+            opcode: self.get_opcode(),
+            payload_length: self.payload_length,
+            mask: if masked { Some(self.mask) } else { None },
+            fin: self.basic_header[0] & 0x80 == 0x80,
+            reserved: (self.basic_header[0] & 0x70) >> 4,
+        }
+    }
+
     /// Call this function again if any of the following conditions are met:
-    /// 
+    ///
     /// * When new incoming data is available on the socket
     /// * When previous invocation of `add_data` returned non-empty `unprocessed_input_data`.
     /// * When previous invocation of `add_data` returned non-`None` `event.
     pub fn add_data<'a, 'b>(
         &'a mut self,
-        data: &'b mut [u8],
-    ) -> Result<WebSocketDecoderAddDataResult<'b>, Error> {
-        todo!()
+        mut data: &'b mut [u8],
+    ) -> WebSocketDecoderAddDataResult<'b> {
+        loop {
+            
+            macro_rules! return_dummy {
+                () => {
+                    return WebSocketDecoderAddDataResult {
+                        unprocessed_input_data: data,
+                        decoded_payload: None,
+                        event: None,
+                    };
+                };
+            }
+            if data.len() == 0 && ! matches!(self.state, FrameDecodingState::PayloadData{remaining: 0, ..}) {
+                return_dummy!();
+            }
+            macro_rules! try_to_fill_buffer_or_return {
+                ($v:ident) => {
+                    data = $v.slurp(data);
+                    if !$v.is_full() {
+                        assert!(data.is_empty());
+                        return_dummy!();
+                    }
+                    let $v = $v.data;
+                };
+            }
+            let mut length_is_ready = false;
+            match self.state {
+                FrameDecodingState::HeaderBeginning(ref mut v) => {
+                    try_to_fill_buffer_or_return!(v);
+                    self.basic_header = v;
+                    match self.basic_header[1] & 0x7F {
+                        0x7E => {
+                            self.state = FrameDecodingState::PayloadLength16(SmallBufWithLen::new())
+                        }
+                        0x7F => {
+                            self.state = FrameDecodingState::PayloadLength64(SmallBufWithLen::new())
+                        }
+                        x => {
+                            self.payload_length = x.into();
+                            length_is_ready = true;
+                        }
+                    };
+                }
+                FrameDecodingState::PayloadLength16(ref mut v) => {
+                    try_to_fill_buffer_or_return!(v);
+                    self.payload_length = u16::from_be_bytes(v).into();
+                    length_is_ready = true;
+                }
+                FrameDecodingState::PayloadLength64(ref mut v) => {
+                    try_to_fill_buffer_or_return!(v);
+                    self.payload_length = u64::from_be_bytes(v);
+                    length_is_ready = true;
+                }
+                FrameDecodingState::MaskingKey(ref mut v) => {
+                    try_to_fill_buffer_or_return!(v);
+                    self.mask = v;
+                    self.state = FrameDecodingState::PayloadData {
+                        phase: Some(NonMaxU8::default()),
+                        remaining: self.payload_length,
+                    };
+                    return WebSocketDecoderAddDataResult {
+                        unprocessed_input_data: data,
+                        decoded_payload: None,
+                        event: Some(WebsocketEvent::FrameStart(self.get_frame_info(true))),
+                    };
+                }
+                FrameDecodingState::PayloadData {
+                    phase,
+                    remaining: 0,
+                } => {
+                    self.state = FrameDecodingState::HeaderBeginning(SmallBufWithLen::new());
+                    return WebSocketDecoderAddDataResult {
+                        unprocessed_input_data: data,
+                        decoded_payload: None,
+                        event: Some(WebsocketEvent::FrameEnd(
+                            self.get_frame_info(phase.is_some()),
+                        )),
+                    };
+                }
+                FrameDecodingState::PayloadData {
+                    ref mut phase,
+                    ref mut remaining,
+                } => {
+                    let mut max_len = data.len();
+                    if let Ok(remaining_usize) = usize::try_from(*remaining) {
+                        max_len = max_len.min(remaining_usize);
+                    }
+                    let (payload_chunk, rest) = data.split_at_mut(max_len);
+
+                    if let Some(phase) = phase {
+                        let mut ph = phase.get();
+                        for b in payload_chunk.iter_mut() {
+                            let index = (ph & 0x03) as usize;
+                            *b ^= self.mask[index];
+                            ph += 1;
+                        }
+                        *phase = NonMaxU8::new(ph & 0x03).unwrap();
+                    }
+
+                    *remaining -= max_len as PayloadLength;
+                    return WebSocketDecoderAddDataResult {
+                        unprocessed_input_data: rest,
+                        decoded_payload: Some(payload_chunk),
+                        event: Some(WebsocketEvent::FramePayloadChunk),
+                    };
+                }
+            }
+            if length_is_ready {
+                if self.basic_header[1] & 0x80 == 0x80 {
+                    self.state = FrameDecodingState::MaskingKey(SmallBufWithLen::new());
+                } else {
+                    self.state = FrameDecodingState::PayloadData {
+                        phase: None,
+                        remaining: self.payload_length,
+                    };
+                    return WebSocketDecoderAddDataResult {
+                        unprocessed_input_data: data,
+                        decoded_payload: None,
+                        event: Some(WebsocketEvent::FrameStart(self.get_frame_info(false))),
+                    };
+                }
+            }
+        }
+    }
+
+    /// There is no incomplete WebSocket frame at this moment and EOF is valid here.
+    ///
+    /// This method is not related to [`Opcode::ConnectionClose`] in any way.
+    pub fn eof_valid(&self) -> bool {
+        matches!(self.state, FrameDecodingState::HeaderBeginning(..))
     }
 
     pub fn new() -> Self {
-        WebSocketDecoder {  }
+        WebSocketFrameDecoder {
+            state: FrameDecodingState::HeaderBeginning(SmallBufWithLen::new()),
+            mask: [0; 4],
+            basic_header: [0; 2],
+            payload_length: 0,
+        }
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Opcode {
+    Continuation = 0,
+    Text = 1,
+    Binary = 2,
+    ReservedData3 = 3,
+    ReservedData4 = 4,
+    ReservedData5 = 5,
+    ReservedData6 = 6,
+    ReservedData7 = 7,
+    ConnectionClose = 8,
+    Ping = 9,
+    Pong = 0xA,
+    ReservedControlB = 0xB,
+    ReservedControlC = 0xC,
+    ReservedControlD = 0xD,
+    ReservedControlE = 0xE,
+    ReservedControlF = 0xF,
 }
 
 #[derive(Debug)]
 pub enum Error {}
 
-#[derive(Debug,PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct FrameInfo {
-    pub payload_length: u64,
+    pub opcode: Opcode,
+    pub payload_length: PayloadLength,
+    pub mask: Option<[u8; 4]>,
+    pub fin: bool,
+    pub reserved: u8,
 }
 
 pub enum WebSocketMessageType {
@@ -67,14 +315,11 @@ pub enum WebSocketMessageType {
 
 /// Events that [`WebSocketEncoder`] consume or [`WebSocketDecoder`] produce.
 /// Does not contain actual payload data - content chunks are delivered (or supplied) as a separate argument
-#[derive(Debug,PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum WebsocketEvent {
-    DataFrameStart(FrameInfo),
-    DataFrameChunk,
-    DataFrameEnd(FrameInfo),
-    ControlFrameStart(FrameInfo),
-    ControlFrameChunk,
-    ControlFrameEnd(FrameInfo),
+    FrameStart(FrameInfo),
+    FramePayloadChunk,
+    FrameEnd(FrameInfo),
 }
 
 /*impl core::error::Error for Error {
--- src/tests.rs
+++ src/tests.rs
@@ -6,14 +6,21 @@ use super::*;
 
 extern crate std;
 
+#[allow(unused)]
+use std::dbg;
+
+use pretty_assertions::assert_eq;
+
 fn decode(input: &[u8]) -> (Vec<u8>, Vec<WebsocketEvent>) {
     let mut input : Vec<u8> = input.into();
     let mut ibuf = &mut input[..];
     let mut payload = Vec::new();
     let mut events = Vec::new();
-    let mut d  = WebSocketDecoder::new();
+    let mut d  = WebSocketFrameDecoder::new();
     loop {
-        let ret = d.add_data(ibuf).unwrap();
+        //dbg!(ibuf.len());
+        let ret = d.add_data(ibuf);
+        //dbg!(&ret);
         ibuf = ret.unprocessed_input_data;
         if let Some(chunk) = ret.decoded_payload {
             payload.extend_from_slice(chunk);
@@ -36,31 +43,58 @@ fn decode_dummy() {
 
 #[test]
 fn decode_simple_unmasked() {
-    assert_eq!(decode(b"\x81\x05\x48\x65\x6c\x6c\x6f"), ((*b"Hello").into(), vec![]));
+    assert_eq!(decode(b"\x81\x05\x48\x65\x6c\x6c\x6f"), 
+    ((*b"Hello").into(), vec![
+        WebsocketEvent::FrameStart(FrameInfo { opcode: Opcode::Text, payload_length: 5, mask: None, fin: true, reserved: 0 }),
+        WebsocketEvent::FramePayloadChunk,
+        WebsocketEvent::FrameEnd(FrameInfo { opcode: Opcode::Text, payload_length: 5, mask: None, fin: true, reserved: 0 }),
+    ]));
 }
 
 
 #[test]
 fn decode_simple_masked() {
-    assert_eq!(decode(b"\x81\x85\x37\xfa\x21\x3d\x7f\x9f\x4d\x51\x58"), ((*b"Hello").into(), vec![]));
+    assert_eq!(decode(b"\x81\x85\x37\xfa\x21\x3d\x7f\x9f\x4d\x51\x58"), ((*b"Hello").into(), vec![
+        WebsocketEvent::FrameStart(FrameInfo { opcode: Opcode::Text, payload_length: 5, mask: Some(*b"\x37\xfa\x21\x3d"), fin: true, reserved: 0 }),
+        WebsocketEvent::FramePayloadChunk,
+        WebsocketEvent::FrameEnd(FrameInfo { opcode: Opcode::Text, payload_length: 5, mask: Some(*b"\x37\xfa\x21\x3d"), fin: true, reserved: 0 }),
+    ]));
 }
 
 #[test]
 fn decode_simple_fragmented() {
-    assert_eq!(decode(b"\x01\x03\x48\x65\x6c\x80\x02\x6c\x6f"), ((*b"Hello").into(), vec![]));
+    assert_eq!(decode(b"\x01\x03\x48\x65\x6c\x80\x02\x6c\x6f"), ((*b"Hello").into(), vec![
+        WebsocketEvent::FrameStart(FrameInfo { opcode: Opcode::Text, payload_length: 3, mask: None, fin: false, reserved: 0 }),
+        WebsocketEvent::FramePayloadChunk,
+        WebsocketEvent::FrameEnd(FrameInfo { opcode: Opcode::Text, payload_length: 3, mask: None, fin: false, reserved: 0 }),
+        WebsocketEvent::FrameStart(FrameInfo { opcode: Opcode::Continuation, payload_length: 2, mask: None, fin: true, reserved: 0 }),
+        WebsocketEvent::FramePayloadChunk,
+        WebsocketEvent::FrameEnd(FrameInfo { opcode: Opcode::Continuation, payload_length: 2, mask: None, fin: true, reserved: 0 }),
+    ]));
 }
 
 #[test]
 fn decode_ping_pong() {
-    assert_eq!(decode(b"\x89\x05\x48\x65\x6c\x6c\x6f\x8a\x85\x37\xfa\x21\x3d\x7f\x9f\x4d\x51\x58"), ((*b"HelloHello").into(), vec![]));
+    assert_eq!(decode(b"\x89\x05\x48\x65\x6c\x6c\x6f\x8a\x85\x37\xfa\x21\x3d\x7f\x9f\x4d\x51\x58"), ((*b"HelloHello").into(), vec![
+        WebsocketEvent::FrameStart(FrameInfo { opcode: Opcode::Ping, payload_length: 5, mask: None, fin: true, reserved: 0 }),
+        WebsocketEvent::FramePayloadChunk,
+        WebsocketEvent::FrameEnd(FrameInfo { opcode: Opcode::Ping, payload_length: 5, mask: None, fin: true, reserved: 0 }),
+        WebsocketEvent::FrameStart(FrameInfo { opcode: Opcode::Pong, payload_length: 5, mask: Some(*b"\x37\xfa\x21\x3d"), fin: true, reserved: 0 }),
+        WebsocketEvent::FramePayloadChunk,
+        WebsocketEvent::FrameEnd(FrameInfo { opcode: Opcode::Pong, payload_length: 5, mask: Some(*b"\x37\xfa\x21\x3d"), fin: true, reserved: 0 }),
+    ]));
 }
 
 #[test]
 fn decode_bin256() {
-    let mut input : Vec<u8> = (*b"\x82\x7E\x0100").into();
+    let mut input : Vec<u8> = (*b"\x82\x7E\x01\x00").into();
     let zeroes = vec![0; 256];
     input.extend_from_slice(&zeroes[..]);
-    assert_eq!(decode(&input), (zeroes, vec![]));
+    assert_eq!(decode(&input), (zeroes, vec![
+        WebsocketEvent::FrameStart(FrameInfo { opcode: Opcode::Binary, payload_length: 256, mask: None, fin: true, reserved: 0 }),
+        WebsocketEvent::FramePayloadChunk,
+        WebsocketEvent::FrameEnd(FrameInfo { opcode: Opcode::Binary, payload_length: 256, mask: None, fin: true, reserved: 0 }),
+    ]));
 }
 
 #[test]
@@ -68,7 +102,11 @@ fn decode_bin64k() {
     let mut input : Vec<u8> = (*b"\x82\x7F\x00\x00\x00\x00\x00\x01\x00\x00").into();
     let zeroes = vec![0; 65536];
     input.extend_from_slice(&zeroes[..]);
-    assert_eq!(decode(&input), (zeroes, vec![]));
+    assert_eq!(decode(&input), (zeroes, vec![
+        WebsocketEvent::FrameStart(FrameInfo { opcode: Opcode::Binary, payload_length: 65536, mask: None, fin: true, reserved: 0 }),
+        WebsocketEvent::FramePayloadChunk,
+        WebsocketEvent::FrameEnd(FrameInfo { opcode: Opcode::Binary, payload_length: 65536, mask: None, fin: true, reserved: 0 }),
+    ]));
 }
 
 
